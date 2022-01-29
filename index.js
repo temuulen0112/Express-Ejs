675c814d2ada0cba363a2c63a64036b1da075250
Mstars_lab1_07 <mstars_lab1_07@Mstars-lab1-07s-iMac.local>
2022-01-29 13:10:28 +0800

    [ADD] express-ejs but not finished

---
+++ index.js
@@ -0,0 +1,43 @@
+const { __express } = require('ejs');
+const express = require('express');
+const { redirect } = require('express/lib/response');
+const app = express();
+const port = 3001;
+app.use(express.json());
+app.use(express.static('public'));
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view option", { layout: false } );
+
+const { quotes } = require('./data.json');
+
+app.use("/quotes", (req, res) => {
+    res.render("quotes", { title: "Quote lists", quotes });
+})
+
+app.use("/quote/:id", (req, res) => {
+    const id = req.params.id;
+    const result = quotes.filter(quotes => quotes.id == id);
+    res.render("quote", { title: "Quote", result})
+})
+
+app.get('/', (req, res) => {
+    res.render('index', {name: 'Temuulen'})
+})
+
+app.get('/404', (req, res) => {
+    res.render('404', {message: 'oopsie!'})
+})
+
+app.get("/", (req, res) => {
+    res.redirect("/quotes")
+})
+
+// app.all('*', (req, res, next) => {
+
+// })
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
